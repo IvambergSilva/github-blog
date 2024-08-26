5f44b3ff6d63114097563fadbe63679605ab9e39
IvambergSilva <ivambergisilva@gmail.com>
2024-08-26 13:09:16 -0300

    feat: add components to the app page

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,21 @@
+import { ThemeProvider } from "styled-components";
+import { BlogContextProvider } from "./context/blog-context";
+import { GlobalStyles } from "./styles/global-styles";
+import { DefaultTheme } from "./styles/theme-default/theme-default";
+import { Home } from "./components/home/home";
+import { DefaultLayout } from "./components/default-layout/default-layout";
+
 export function App() {
     return (
-        <>GitHub Blog</>
+        <>
+            <ThemeProvider theme={DefaultTheme}>
+                <BlogContextProvider>
+                    <DefaultLayout>
+                        <Home />
+                    </DefaultLayout>
+                    <GlobalStyles />
+                </BlogContextProvider>
+            </ThemeProvider>
+        </>
     )
 }
